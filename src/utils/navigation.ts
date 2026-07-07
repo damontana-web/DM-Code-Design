dcad2573c40fd3a724094a50f1f62ceeca9ef21b
damontana-web <david.montana@healthscope.com.au>
2026-07-08 09:59:43 +1000

    content

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -24,8 +24,17 @@ const footerLinks = [
     ],
   },
 ];
+// An object of links for social icons
+const socialLinks = {
+  facebook: "https://www.facebook.com/",
+  x: "https://twitter.com/",
+  github: "https://github.com/mearashadowfax/ScrewFast",
+  google: "https://www.google.com/",
+  slack: "https://slack.com/",
+};
 
 export default {
   navBarLinks,
   footerLinks,
+  socialLinks,
 };
